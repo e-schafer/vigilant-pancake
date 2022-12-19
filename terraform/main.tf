57b23597b754e1b8d20d6b7def87efd218b4ef98
SCHÄFER Eric <schafer490+github@gmail.com>
2022-12-19 15:09:28 +0100

    feat(terraform): init a synapse workspace

---
+++ terraform/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "vollgaz_synapse_rg" {
+  name     = "vollgaz-synapse-rg"
+  location = "West Europe"
+  tags = {
+    creator = "eric schäfer",
+    usage   = "demo"
+  }
+}
+
+resource "azurerm_storage_account" "vollgaz_synapse_storacc" {
+  name                      = "vollgazsynapse"
+  resource_group_name       = azurerm_resource_group.vollgaz_synapse_rg.name
+  location                  = azurerm_resource_group.vollgaz_synapse_rg.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
+  is_hns_enabled            = true
+  enable_https_traffic_only = true
+
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "vollgaz_synapse_azdl" {
+  name               = "vollgaz-synapse-azdl"
+  storage_account_id = azurerm_storage_account.vollgaz_synapse_storacc.id
+
+}
+
+resource "azurerm_synapse_workspace" "vollgaz_synapse_workspace" {
+  name                                 = "vollgaz-synapse-workspace"
+  resource_group_name                  = azurerm_resource_group.vollgaz_synapse_rg.name
+  location                             = azurerm_resource_group.vollgaz_synapse_rg.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.vollgaz_synapse_azdl.id
+  sql_administrator_login              = "sqladminuser"
+  sql_administrator_login_password     = "H@Sh1CoR3!"
+  managed_virtual_network_enabled      = true
+
+  public_network_access_enabled = false
+
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
